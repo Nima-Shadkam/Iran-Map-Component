5bbc83c51a0289af199d08dccae6028ef5ca665b
Nima <93921089+nimashadkam@users.noreply.github.com>
2023-03-03 17:28:11 +0330

    Updated Files

---
+++ IranMap/js/citites.js
@@ -0,0 +1,49 @@
+
+/// Central Provinces - Location
+let dataLocation = [
+    {id: "1", city: "تهران" ,latitude:35.8118758,longitude:51.3920875   },
+    {id: "2", city: "کرج" ,  latitude:35.854101,longitude:50.984519   },
+    {id: "3", city: "قزوین" ,latitude:36.32134,longitude:50.036382   },
+    {id: "4", city: "شیراز" ,latitude:29.763067,longitude:52.481595   },
+    {id: "5", city: "قم" ,  latitude:34.574594,longitude:50.80728   },
+    {id: "6", city: "زنجان" ,  latitude:36.671654,longitude:48.505726   },
+    {id: "7", city: "همدان" ,  latitude:34.844855,longitude:48.559996   },
+    {id: "8", city: "کرمان" ,  latitude:30.241704,longitude:57.077368   },
+    {id: "9", city: "سمنان" ,  latitude:35.606907,longitude:53.44083   },
+    {id: "10", city: "مشهد" , latitude:35.662642,longitude:59.842748   },
+    {id: "11", city: "اصفهان" , latitude:32.672228,longitude:51.391638   },
+    {id: "12", city: "اراک" , latitude:34.054785,longitude:49.803239   },
+    {id: "13", city: "اردبیل" , latitude:38.228487,longitude:48.27276   },
+    {id: "14", city: "ارومیه" , latitude:36.165615,longitude:45.471305   },
+    {id: "15", city: "اهواز" , latitude:31.284294,longitude:48.621685   },
+    {id: "16", city: "ایلام" , latitude:33.637636,longitude:46.381969   },
+    {id: "17", city: "بجنورد" , latitude:37.452277,longitude:57.323118   },
+    {id: "18", city: "بندرعباس" , latitude:27.208631,longitude:56.335691   },
+    {id: "19", city: "بوشهر" , latitude:28.935077,longitude:51.075429   },
+    {id: "20", city: "بیرجند" , latitude:32.837711,longitude:59.234827   },
+    {id: "21", city: "تبریز" , latitude:38.044189,longitude:46.388512   },
+    {id: "22", city: "خرم آباد" , latitude:33.481426,longitude:48.427468   },
+    {id: "23", city: "رشت" , latitude:37.243658,longitude:49.577674   },
+    {id: "24", city: "ساری" , latitude:36.648396,longitude:53.070134   },
+    {id: "25", city: "سنندج" , latitude:35.302555,longitude:47.002252   },
+    {id: "26", city: "شهرکرد" , latitude:32.351325,longitude:50.900723   },
+    {id: "27", city: "کرمانشاه" , latitude:34.289818,longitude:47.027199   },
+    {id: "28", city: "گرگان" , latitude:36.863387,longitude:54.448579   },
+    {id: "29", city: "یاسوج" , latitude:30.710857,longitude:51.556916   },
+    {id: "30", city: "یزد" , latitude:31.839473,longitude:54.351585   },
+ 
+  
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ IranMap/js/mapdata.js
@@ -0,0 +1,595 @@
+﻿var simplemaps_countrymap_mapdata = {
+  main_settings: {
+    //General settings
+    width: "500", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+
+    //State defaults
+    state_description: "State description",
+    state_color: "#2279c3",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: "2",
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "image", //"square",
+    // location_image_source: "",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+
+
+  /// ToolTip For Province
+  state_specific: {
+    IRN3209: {
+      name: "کهکیویه و بویر احمد",
+      description: "",
+      val: "10",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3210: {
+      name: "بوشهر",
+      description: " ",
+      val: "20",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3211: {
+      name: "اصفهان",
+      description: " ",
+      val: "30",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3212: {
+      name: "فارس",
+      description: " ",
+      val: "40",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3213: {
+      name: "گلستان",
+      description: " ",
+      val: "40",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3214: {
+      name: "مازندران",
+      description: " ",
+      val: "50",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3215: {
+      name: "سمنان",
+      description: " ",
+      val: "60",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3216: {
+      name: "تهران",
+      description: " ",
+      val: "70",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3217: {
+      name: "یزد",
+      description: " ",
+      val: "80",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3218: {
+      name: " چهارمحال بختیاری",
+      description: " ",
+      val: "90",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3219: {
+      name: "خوزستان",
+      description: " ",
+      val: "10",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3220: {
+      name: "لرستان",
+      description: " ",
+      val: "110",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3221: {
+      name: "ایلام",
+      description: " ",
+      val: "120",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3228: {
+      name: "هرمزگان",
+      description: " ",
+      val: "130",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3230: {
+      name: "اردبیل",
+      description: " ",
+      val: "140",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3231: {
+      name: "مرکزی",
+      description: " ",
+      val: "150",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3232: {
+      name: "قم",
+      description: " ",
+      val: "160",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3233: {
+      name: "همدان",
+      description: " ",
+      val: "170",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3234: {
+      name: "زنجان",
+      description: " ",
+      val: "180",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3235: {
+      name: "قزوین",
+      description: " ",
+      val: "190",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3237: {
+      name: "آذربایجان غربی",
+      description: " ",
+      val: "200",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3238: {
+      name: "آذربایجان شرقی",
+      description: " ",
+      val: "210",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3239: {
+      name: "کرمانشاه",
+      description: " ",
+      val: "220",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3240: {
+      name: "گیلان",
+      description: " ",
+      val: "230",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3241: {
+      name: "کردستان",
+      description: " ",
+      val: "240",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3244: {
+      name: "خرسان جنوبی ",
+      description: " ",
+      val: "250",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3245: {
+      name: "خراسان رضوی",
+      description: " ",
+      val: "260",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3246: {
+      name: "خرسان شمالی",
+      description: " ",
+      val: "270",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3247: {
+      name: "سیستان و بلوچستان",
+      description: " ",
+      val: "280",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN3248: {
+      name: "کرمان",
+      description: " ",
+      val: "290",
+      color: 'default',
+      hover_color: 'default',
+    },
+    IRN5490: {
+      name: "البرز",
+      description: " ",
+      val: "300",
+      color: 'default',
+      hover_color: 'default',
+    }
+  },
+
+  labels: {
+    "0": {
+      name: "کهگیلویه وبویراحمد ",
+      parent_id: "IRN3209",
+      val: "31",
+      image_url: "",
+
+      x: 350.4,
+      y: 570.8
+    },
+    "1": {
+      name: "بوشهر",
+      parent_id: "IRN3210",
+      val: "17",
+      image_url: "",
+
+      x: 390.3,
+      y: 702.2
+    },
+    "2": {
+      name: "اصفهان",
+      parent_id: "IRN3211",
+      val: "5",
+      image_url: "",
+
+      x: 418.8,
+      y: 414
+    },
+    "3": {
+      name: "فارس",
+      parent_id: "IRN3212",
+      val: "2",
+      image_url: "",
+
+      x: 496.1,
+      y: 701.1
+    },
+    "4": {
+      name: "گلستان",
+      parent_id: "IRN3213",
+      val: "28",
+      image_url: "",
+
+      x: 586.7,
+      y: 150.5
+    },
+    "5": {
+      name: "مازندران",
+      parent_id: "IRN3214",
+      val: "585",
+      image_url: "",
+
+      x: 470.5,
+      y: 227
+    },
+    "6": {
+      name: "سمنان",
+      parent_id: "IRN3215",
+      val: "11",
+      image_url: "",
+
+      x: 572.5,
+      y: 279.9
+    },
+    "7": {
+      name: "تهران",
+      parent_id: "IRN3216",
+      val: "9",
+      image_url: "",
+
+      x: 382.4,
+      y: 277.2
+    },
+    "8": {
+      name: "یزد",
+      parent_id: "IRN3217",
+      val: "29",
+      image_url: "",
+
+      x: 633.6,
+      y: 439.2
+    },
+    "9": {
+      name: " چهارمحال وبختیاری  ",
+      parent_id: "IRN3218",
+      val: "27",
+      image_url: "",
+
+      x: 348.7,
+      y: 502.1
+    },
+    "10": {
+      name: "خوزستان",
+      parent_id: "IRN3219",
+      val: "7",
+      image_url: "",
+
+      x: 256.1,
+      y: 516.2
+    },
+    "11": {
+      name: "لرستان",
+      parent_id: "IRN3220",
+      val: "26",
+      image_url: "",
+
+      x: 204.7,
+      y: 402.9
+    },
+    "12": {
+      name: "ایلام",
+      parent_id: "IRN3221",
+      val: "23",
+      image_url: "",
+
+      x: 134.2,
+      y: 421.9
+    },
+    "13": {
+      name: "هرمزگان",
+      parent_id: "IRN3228",
+      val: "16",
+      image_url: "",
+
+      x: 625.9,
+      y: 756.2
+    },
+    "14": {
+      name: "اردبیل",
+      parent_id: "IRN3230",
+      val: "20",
+      image_url: "",
+
+      x: 215.7,
+      y: 101.2
+    },
+    "15": {
+      name: "مرکزی",
+      parent_id: "IRN3231",
+      val: "6",
+      image_url: "",
+
+      x: 292.1,
+      y: 356.1
+    },
+    "16": {
+      name: "قم",
+      parent_id: "IRN3232",
+      val: "19",
+      image_url: "",
+
+      x: 369.3,
+      y: 323.2
+    },
+    "17": {
+      name: "همدان",
+      parent_id: "IRN3233",
+      val: "24",
+      image_url: "",
+
+      x: 241.9,
+      y: 304.4
+    },
+    "18": {
+      name: "زنجان",
+      parent_id: "IRN3234",
+      val: "21",
+      image_url: "",
+
+      x: 227.3,
+      y: 204.2
+    },
+    "19": {
+      name: "قزوین",
+      parent_id: "IRN3235",
+      val: "25",
+      image_url: "",
+
+      x: 306.1,
+      y: 239.4
+    },
+    "20": {
+      name: "آذربایجان غربی ",
+      parent_id: "IRN3237",
+      val: "3",
+      image_url: "",
+
+      x: 42.5,
+      y: 61.3
+    },
+    "21": {
+      name: "آذربایجان شرقی ",
+      parent_id: "IRN3238",
+      val: "14",
+      image_url: "",
+
+      x: 118.7,
+      y: 126.3
+    },
+    "22": {
+      name: "کرمانشاه",
+      parent_id: "IRN3239",
+      val: "12",
+      image_url: "",
+
+      x: 116.5,
+      y: 346
+    },
+    "23": {
+      name: "گیلان",
+      parent_id: "IRN3240",
+      val: "18",
+      image_url: "",
+
+      x: 298.7,
+      y: 182.3
+    },
+    "24": {
+      name: "کردستان",
+      parent_id: "IRN3241",
+      val: "22",
+      image_url: "",
+
+      x: 159.3,
+      y: 265.4
+    },
+    "25": {
+      name: "خراسان جنوبی ",
+      parent_id: "IRN3244",
+      val: "32",
+      image_url: "",
+
+      x: 801.6,
+      y: 486.2
+    },
+    "26": {
+      name: "خراسان رضوی ",
+      parent_id: "IRN3245",
+      val: "10",
+      image_url: "",
+
+      x: 786.9,
+      y: 272.4
+    },
+    "27": {
+      name: "خراسان شمالی ",
+      parent_id: "IRN3246",
+      val: "30",
+      image_url: "",
+
+      x: 668.9,
+      y: 151.6
+    },
+    "28": {
+      name: "سیستان وبلوچستان  ",
+      parent_id: "IRN3247",
+      val: "15",
+      image_url: "",
+
+      x: 860.9,
+      y: 771.4
+    },
+    "29": {
+      name: "کرمان",
+      parent_id: "IRN3248",
+      val: "8",
+      image_url: "",
+
+      x: 695.4,
+      y: 618.5
+    },
+    "30": {
+      name: "البرز",
+      parent_id: "IRN5490",
+      val: "13",
+      image_url: "",
+      x: 355.7,
+      y: 243.5
+    },
+
+
+
+  },
+
+  locations: {
+    // "0": {
+    //   lat: "35.705",
+    //   lng: "51.4216",
+    //   name: "تهران"
+    // },
+    // "1": {
+    //   lat: "38.044",
+    //   lng: "46.389",
+    //   name: "تبریز"
+    // }
+  },
+
+  regions: {}
+};
